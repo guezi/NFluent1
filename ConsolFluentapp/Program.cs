ae3ae86a4dc9145c55ab4b6219c8bd01025a0691
ferhat <guezif@yahoo.fr>
2018-02-26 21:16:21 +0100

    fisrt commit

---
+++ ConsolFluentapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolFluentapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
